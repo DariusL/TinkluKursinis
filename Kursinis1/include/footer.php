70a3ade88f70d2cac1a35e93da075c1be87ee9d8
DariusL <d.lapunas@gmail.com>
2014-03-21 19:19:54 +0200

    Logout footeryje

--- Kursinis1/include/footer.php
+++ Kursinis1/include/footer.php
@@ -6,10 +6,20 @@
  * and how many guests viewing site. 
  */
 if (isset($database)) {
+    $log_out = "";
+    if(isset($session) && $session->logged_in){
+        $path = "";
+        if (isset($_SESSION['path'])) {
+            $path = $_SESSION['path'];
+            unset($_SESSION['path']);
+        }
+        $log_out = "<a href=\"" . $path . "process.php\">Atsijungti</a> &nbsp;&nbsp;";
+    }
     echo ""
     . "<table width=100% "
     . "style=\"padding:1px;background-color:#DCDCDC;border:1px dashed grey;\">\n"
     . "<tr align=\"center\"><td>\n"
+    . $log_out
     . "<b>Registruotų vartotojų kiekis: </b> " . $database->getNumMembers() . ".&nbsp"
     . "</td></tr>\n"
     . "</table>\n"
