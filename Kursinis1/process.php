7fff8a6d66c464b9fbbddc01cc514ec4b6278b0f
DariusL <d.lapunas@gmail.com>
2014-03-22 18:12:37 +0200

    Cleanup

--- Kursinis1/process.php
+++ Kursinis1/process.php
@@ -11,15 +11,9 @@ class Process {
         if (isset($_POST['sublogin'])) {
             $this->procLogin();
         }
-        /* User submitted registration form */ else if (isset($_POST['subjoin'])) {
+        /* User submitted registration form */ 
+        else if (isset($_POST['subjoin'])) {
             $this->procRegister();
-        }
-        
-        /* User submitted forgot password form */ else if (isset($_POST['subforgot'])) {
-            $this->procForgotPass();
-        }
-        /* User submitted edit account form */ else if (isset($_POST['subedit'])) {
-            $this->procEditAccount();
         }
         /**
          * The only other reason user should be directed here
@@ -99,77 +93,6 @@ class Process {
         /* Registration attempt failed */ else if ($retval == 2) {
             $_SESSION['reguname'] = $_POST['id'];
             $_SESSION['regsuccess'] = false;
-            header("Location: " . $session->referrer);
-        }
-    }
-
-    /**
-     * procForgotPass - Validates the given username then if
-     * everything is fine, a new password is generated and
-     * emailed to the address the user gave on sign up.
-     */
-    function procForgotPass() {
-        global $database, $session, $mailer, $form;
-        /* Username error checking */
-        $subuser = $_POST['user'];
-        $field = "user";  //Use field name for username
-        if (!$subuser || strlen($subuser = trim($subuser)) == 0) {
-            $form->setError($field, "* Neįvestas vartotojo vardas<br>");
-        } else {
-            /* Make sure username is in database */
-            $subuser = stripslashes($subuser);
-            if (strlen($subuser) < 5 || strlen($subuser) > 30 ||
-                    !eregi("^([0-9a-z])+$", $subuser) ||
-                    (!$database->usernameTaken($subuser))) {
-                $form->setError($field, "* Vartotojas neegzistuoja<br>");
-            }
-        }
-
-        /* Errors exist, have user correct them */
-        if ($form->num_errors > 0) {
-            $_SESSION['value_array'] = $_POST;
-            $_SESSION['error_array'] = $form->getErrorArray();
-        }
-        /* Generate new password and email it to user */ else {
-            /* Generate new password */
-            $newpass = $session->generateRandStr(8);
-
-            /* Get email of user */
-            $usrinf = $database->getUserInfo($subuser);
-            $email = $usrinf['email'];
-
-            /* Attempt to send the email with new password */
-            if ($mailer->sendNewPass($subuser, $email, $newpass)) {
-                /* Email sent, update database */
-                $database->updateUserField($subuser, "password", md5($newpass));
-                $_SESSION['forgotpass'] = true;
-            }
-            /* Email failure, do not change password */ else {
-                $_SESSION['forgotpass'] = false;
-            }
-        }
-
-        header("Location: " . $session->referrer);
-    }
-
-    /**
-     * procEditAccount - Attempts to edit the user's account
-     * information, including the password, which must be verified
-     * before a change is made.
-     */
-    function procEditAccount() {
-        global $session, $form;
-        /* Account edit attempt */
-        $retval = $session->editAccount($_POST['curpass'], $_POST['newpass'], $_POST['email']);
-
-        /* Account edit successful */
-        if ($retval) {
-            $_SESSION['useredit'] = true;
-            header("Location: " . $session->referrer);
-        }
-        /* Error found with form */ else {
-            $_SESSION['value_array'] = $_POST;
-            $_SESSION['error_array'] = $form->getErrorArray();
             header("Location: " . $session->referrer);
         }
     }
